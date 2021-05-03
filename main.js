991e45e80ce3ddbf1d69ee5cadc9b98df6d9e5fe
Cargalher <cargalher@gmail.com>
2021-05-03 19:08:05 +0200

    parte visiva

---
+++ main.js
@@ -0,0 +1,28 @@
+// calculate ticket price
+
+// Ask customer Number of kms
+// var kilometers = prompt('How far would you travel?');
+// console.log(kilometers);
+
+// // Ask customer Age
+// var age = prompt('How old are you?');
+// console.log(age);
+
+// // General Ticket price
+// var tPrice = kilometers * 0.21;
+// console.log(tPrice);
+
+// //Prices with and without Discounts
+
+// if ( age < 18 ){
+//     tPrice -= tPrice * 0.20;
+//     var rounded_price = tPrice.toFixed(2);
+//     document.getElementById('info').innerHTML = 'The price of your ticket is ' + ': ' + rounded_price + ' €';
+// }else if ( age > 65 ){
+//     tPrice -= tPrice * 0.40;
+//     var rounded_price = tPrice.toFixed(2);
+//     document.getElementById('info').innerHTML = 'The price of your ticket is ' + ': ' + rounded_price + ' €';
+// }else{
+//     var rounded_price = tPrice.toFixed(2);
+//     document.getElementById('info').innerHTML = 'The price of your ticket is ' + ': ' + rounded_price + ' €';
+//  }
