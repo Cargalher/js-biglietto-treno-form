712f3dbc063c030bc643c493fcea66f55175455e
Cargalher <cargalher@gmail.com>
2021-05-04 00:50:45 +0200

    Swlwct input for age

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-// calculate ticket price
+// // calculate ticket price
 
-// Ask customer Number of kms
+// // Ask customer Number of kms
 // var kilometers = prompt('How far would you travel?');
 // console.log(kilometers);
 
-// // Ask customer Age
+// // // Ask customer Age
 // var age = prompt('How old are you?');
 // console.log(age);
 
-// // General Ticket price
+// // // General Ticket price
 // var tPrice = kilometers * 0.21;
 // console.log(tPrice);
 
-// //Prices with and without Discounts
+// // //Prices with and without Discounts
 
 // if ( age < 18 ){
 //     tPrice -= tPrice * 0.20;
@@ -26,3 +26,5 @@
 //     var rounded_price = tPrice.toFixed(2);
 //     document.getElementById('info').innerHTML = 'The price of your ticket is ' + ': ' + rounded_price + ' €';
 //  }
+
+ 
