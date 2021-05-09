317a7d22a41366b1deda30fdc3960a3c0d165124
Cargalher <cargalher@gmail.com>
2021-05-09 17:22:22 +0200

    finished form ticket train

--- main.js
+++ main.js
@@ -1,30 +1,80 @@
-// // calculate ticket price
+/* 
+Creiamo un finto biglietto del treno con:
+Nome passeggero
+Codice treno (numero casuale tra 90000 e 100000 escluso)
+Numero carrozza
+Prezzo calcolato
+Categoria selezionata dall'utente
+Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", 
+se clicchiamo su annulla dobbiamo ripulire il form.
+*/
 
-// // Ask customer Number of kms
-// var kilometers = prompt('How far would you travel?');
-// console.log(kilometers);
 
-// // // Ask customer Age
-// var age = prompt('How old are you?');
-// console.log(age);
+document.getElementById('createTicket').addEventListener('click', createTicket);
 
-// // // General Ticket price
-// var tPrice = kilometers * 0.21;
-// console.log(tPrice);
+document.getElementById('clearTicket').addEventListener('click', clearTicket);
 
-// // //Prices with and without Discounts
 
-// if ( age < 18 ){
-//     tPrice -= tPrice * 0.20;
-//     var rounded_price = tPrice.toFixed(2);
-//     document.getElementById('info').innerHTML = 'The price of your ticket is ' + ': ' + rounded_price + ' €';
-// }else if ( age > 65 ){
-//     tPrice -= tPrice * 0.40;
-//     var rounded_price = tPrice.toFixed(2);
-//     document.getElementById('info').innerHTML = 'The price of your ticket is ' + ': ' + rounded_price + ' €';
-// }else{
-//     var rounded_price = tPrice.toFixed(2);
-//     document.getElementById('info').innerHTML = 'The price of your ticket is ' + ': ' + rounded_price + ' €';
-//  }
 
- 
+function createTicket () {
+
+    //Printing on the ticket the name of the passeger
+    document.getElementById('client_name').innerHTML = `${fullName.value}`;
+
+    //Customer Number of kms
+    var distance = document.getElementById('distance').value;
+
+    //Customer Age
+    var age = document.getElementById('age_group').value;
+
+    //General Ticket price
+    var tPrice = distance * 0.21;
+    console.log(tPrice);
+
+    //Prices with and without Discounts
+    if ( age === 'minor' ){
+        tPrice = tPrice - (tPrice * 0.20);
+        var rounded_price = tPrice.toFixed(2);
+
+        document.getElementById('ticketPrice').innerHTML = `${rounded_price}`;
+        document.getElementById('discount').innerHTML = "Sconto minori";
+
+    }else if ( age === 'senior' ){
+        tPrice = tPrice - (tPrice * 0.40);
+        var rounded_price = tPrice.toFixed(2);
+
+        document.getElementById('ticketPrice').innerHTML = `${rounded_price}`;
+        document.getElementById('discount').innerHTML = "Sconto anziani";
+
+    }else if ( age === 'adult'){
+        var rounded_price = tPrice.toFixed(2);
+    
+        document.getElementById('ticketPrice').innerHTML = `${rounded_price}`;
+        document.getElementById('discount').innerHTML = " ";
+
+    }
+    //Printing the carriage number on the ticket 
+    document.getElementById('carriageNumber').innerHTML = `${generateCarriageNumber(1, 15)}`;
+
+    //Printing on the ticket the train code 
+    document.getElementById('code').innerHTML = `${generateTrainCode(90000, 100000)}`;
+}
+
+
+
+function clearTicket () {
+    document.getElementById('client_name').innerHTML = " ";
+    document.getElementById('discount').innerHTML = " ";
+    document.getElementById('code').innerHTML = " ";
+    document.getElementById('ticketPrice').innerHTML = " ";
+    document.getElementById('carriageNumber').innerHTML = " ";
+
+}
+
+function generateTrainCode (min, max) {
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+function generateCarriageNumber (min, max) {
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
